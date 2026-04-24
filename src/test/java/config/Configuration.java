7dbe3e321d9fde143ca56c7ae8fcaf12fc8d8a25
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-24 17:11:13 +0300

    config(props): add Configuration interface and config.properties

---
+++ src/test/java/config/Configuration.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources("classpath:configurations/config.properties")
+public interface Configuration extends Config{
+    String baseUrl();
+}
