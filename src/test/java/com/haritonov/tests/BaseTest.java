b8b20f9d865f0b10738395ae535e6aff76774014
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-25 16:03:38 +0300

    fix(base): rename specificationRequest to requestSpecification

--- src/test/java/com/haritonov/tests/BaseTest.java
+++ src/test/java/com/haritonov/tests/BaseTest.java
@@ -9,7 +9,7 @@ import org.aeonbits.owner.ConfigFactory;
 
 public abstract class BaseTest {
     protected static final Configuration config = ConfigFactory.create(Configuration.class, System.getenv());
-    protected static final RequestSpecification specificationRequest = new RequestSpecBuilder()
+    protected static final RequestSpecification requestSpecification = new RequestSpecBuilder()
             .setBaseUri(config.baseUrl())
             .setContentType(ContentType.JSON)
             .setAccept(ContentType.JSON)
