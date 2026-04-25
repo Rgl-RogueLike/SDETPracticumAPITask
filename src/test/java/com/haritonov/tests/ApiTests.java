a8bfa326b2388d5bd6725974bb1880a7253c235f
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-25 18:01:25 +0300

    feat(tests): implement update test covering all entity fields

--- src/test/java/com/haritonov/tests/ApiTests.java
+++ src/test/java/com/haritonov/tests/ApiTests.java
@@ -45,4 +45,36 @@ public class ApiTests extends BaseTest{
         Assertions.assertNotNull(response.getEntity(), "Список не должен быть null");
         Assertions.assertFalse(response.getEntity().isEmpty(), "Список не должен быть пустым");
     }
+
+    @Test
+    @DisplayName("Update entity")
+    public void testPatchEntity() {
+        Integer createId = entitySteps.createEntity(requestSpecification, config.createUrl(),
+                config.lowerLimitSizeImportantNumbers(), config.upperLimitSizeImportantNumbers(),
+                config.lowerNumber(), config.upperNumber());
+
+        EntityResponse entityBeforeUpdate = entitySteps.getEntity(requestSpecification, createId, config.getUrl());
+
+        entitySteps.updateEntity(requestSpecification, config.patchUrl(), createId, !entityBeforeUpdate.getVerified(),
+                config.lowerLimitSizeImportantNumbers(), config.upperLimitSizeImportantNumbers(),
+                config.lowerNumber(), config.upperNumber());
+
+        EntityResponse entityAfterUpdate = entitySteps.getEntity(requestSpecification, createId, config.getUrl());
+
+        Assertions.assertNotNull(entityBeforeUpdate.getAddition(), "Addition не должен быть null после создания");
+        Assertions.assertNotEquals(entityBeforeUpdate.getTitle(), entityAfterUpdate.getTitle(), "Title не обновился");
+        Assertions.assertNotSame(entityBeforeUpdate.getVerified(), entityAfterUpdate.getVerified(), "Verified не обновился");
+
+        Assertions.assertNotEquals(entityBeforeUpdate.getImportantNumbers(),
+                entityAfterUpdate.getImportantNumbers(),
+                "ImportantNumbers не обновилось");
+
+        Assertions.assertNotEquals(entityBeforeUpdate.getAddition().getAdditionalInfo(),
+                entityAfterUpdate.getAddition().getAdditionalInfo(),
+                "AdditionalInfo не обновилась");
+
+        Assertions.assertNotEquals(entityBeforeUpdate.getAddition().getAdditionalNumber(),
+                entityAfterUpdate.getAddition().getAdditionalNumber(),
+                "AdditionNumber не обновилось");
+    }
 }
