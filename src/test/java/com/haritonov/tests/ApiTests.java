97a0e334df63d9031f550d1082a08a8821af262a
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-25 17:14:33 +0300

    feat(tests): add GetAll test case and apply DisplayNames

--- src/test/java/com/haritonov/tests/ApiTests.java
+++ src/test/java/com/haritonov/tests/ApiTests.java
@@ -1,7 +1,9 @@
 package com.haritonov.tests;
 
+import com.haritonov.dto.EntityFilterResponse;
 import com.haritonov.dto.EntityResponse;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.haritonov.steps.EntitySteps;
 
@@ -10,6 +12,7 @@ public class ApiTests extends BaseTest{
     private static final EntitySteps entitySteps = new EntitySteps();
 
     @Test
+    @DisplayName("Create entity")
     public void testCreateEntity() {
         Integer id = entitySteps.createEntity(requestSpecification, config.createUrl(),
                 config.lowerLimitSizeImportantNumbers(), config.upperLimitSizeImportantNumbers(),
@@ -19,6 +22,7 @@ public class ApiTests extends BaseTest{
     }
 
     @Test
+    @DisplayName("Get entity and check data")
     public void testGetEntity() {
         Integer createdId = entitySteps.createEntity(requestSpecification, config.createUrl(),
                 config.lowerLimitSizeImportantNumbers(), config.upperLimitSizeImportantNumbers(),
@@ -28,5 +32,17 @@ public class ApiTests extends BaseTest{
 
         Assertions.assertEquals(createdId, response.getId(), "Id должен совпадать");
         Assertions.assertNotNull(response.getTitle(), "Title не должен быть null");
+    }
+
+    @Test
+    @DisplayName("Get all entities")
+    public void testGetAllEntities() {
+        entitySteps.createEntity(requestSpecification, config.createUrl(),
+                config.lowerLimitSizeImportantNumbers(), config.upperLimitSizeImportantNumbers(),
+                config.lowerNumber(), config.upperNumber());
+
+        EntityFilterResponse response = entitySteps.getAllEntities(requestSpecification, config.getAllUrl());
+        Assertions.assertNotNull(response.getEntity(), "Список не должен быть null");
+        Assertions.assertFalse(response.getEntity().isEmpty(), "Список не должен быть пустым");
     }
 }
