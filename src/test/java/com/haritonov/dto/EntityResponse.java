5449f72848cb018563d263fb454068204ead4503
Rgl-RogueLike <nikita.harit@mail.ru>
2026-04-25 16:02:30 +0300

    fix(dto): fix EntityResponse deserialization

--- src/test/java/com/haritonov/dto/EntityResponse.java
+++ src/test/java/com/haritonov/dto/EntityResponse.java
@@ -1,11 +1,17 @@
 package com.haritonov.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Value
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EntityResponse {
 
     Integer id;
